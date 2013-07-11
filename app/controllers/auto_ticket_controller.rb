23f30bfd969402ceb8c3e512e7e00edd7bb0e33d
kk_Ataka <kk_ataka@ring.skr.jp>
2013-07-11 22:45:03 +0900

    svnとgitリポジトリの場合コミットログを取ってくるようにした

--- app/controllers/auto_ticket_controller.rb
+++ app/controllers/auto_ticket_controller.rb
@@ -4,13 +4,39 @@ class AutoTicketController < ApplicationController
   unloadable
   def index
     puts "call index"
-
-    adapter = Redmine::Scm::Adapters::SubversionAdapter
     project_name = Project.find(params[:id])
 
     log_xml = ""
-    project_name.repositories.each do |repo|
-       log_xml = `svn log --xml #{repo.url}`
+    repositories = project_name.repositories
+    if repositories.empty? then
+      @nothing = "repository is nothing..."
+      return
+    end
+
+    repositories.each do |repository|
+       require 'pp'; pp repository
+       # only default scm
+       next unless repository["is_default"]
+
+       case repository["type"]
+       when Repository::Subversion.to_s
+         log = `svn log --xml #{repository.url}`
+         @log = parse(log)
+         return
+       when Repository::Git.to_s
+         splitter = "||"
+     log = {}
+     pre = `git --git-dir=#{repository.url} log --pretty=format:"%h#{splitter}%s"`
+     pre.split("\n").each do |l|
+           k,v = l.split(splitter)
+       log[k] = v
+     end
+     @log = log
+         return
+       else
+         @nothing = "It have support only Subversion"
+         return
+       end
     end
     @log = parse(log_xml)
   end
@@ -18,7 +44,6 @@ class AutoTicketController < ApplicationController
   def add
     puts "call add"
   end
-
 
 private
   def parse(xml)
