92075d1122b022c2cee9a0b52044fd450d4b679e
kk_Ataka <kk_ataka@ring.skr.jp>
2013-07-11 23:48:11 +0900

    ターゲットチケットまでは絞り込めた。あとプロジェクトリンクを表示するように(find_project)、選択中のページがわかるように(menu_item)した

--- app/controllers/auto_ticket_controller.rb
+++ app/controllers/auto_ticket_controller.rb
@@ -2,19 +2,24 @@ require 'nokogiri'
 
 class AutoTicketController < ApplicationController
   unloadable
+
+  before_filter :find_project
+  menu_item :exec_add
+
+  helper :issues
+  include IssuesHelper
+
   def index
     puts "call index"
-    project_name = Project.find(params[:id])
 
     log_xml = ""
-    repositories = project_name.repositories
+    repositories = @project.repositories
     if repositories.empty? then
       @nothing = "repository is nothing..."
       return
     end
 
     repositories.each do |repository|
-       require 'pp'; pp repository
        # only default scm
        next unless repository["is_default"]
 
@@ -22,16 +27,23 @@ class AutoTicketController < ApplicationController
        when Repository::Subversion.to_s
          log = `svn log --xml #{repository.url}`
          @log = parse(log)
+         @log.each do |revision, log|
+           tickets_no = log.scan(/#[1-9]*/)
+           next if tickets_no.empty?
+           tickets_no.each do |ticket_no|
+             puts "kono ticket no [#{ticket_no}] ni revision no log [r#{revision}: #{log}]"
+           end
+         end
          return
        when Repository::Git.to_s
          splitter = "||"
-     log = {}
-     pre = `git --git-dir=#{repository.url} log --pretty=format:"%h#{splitter}%s"`
-     pre.split("\n").each do |l|
+         log = {}
+         pre = `git --git-dir=#{repository.url} log --pretty=format:"%h#{splitter}%s"`
+         pre.split("\n").each do |l|
            k,v = l.split(splitter)
-       log[k] = v
-     end
-     @log = log
+           log[k] = v
+         end
+         @log = log
          return
        else
          @nothing = "It have support only Subversion"
@@ -55,5 +67,11 @@ private
       end
     end
     return log
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render_404
   end
 end
