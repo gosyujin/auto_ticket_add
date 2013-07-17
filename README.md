66c400c61dc1fd4e6bb764b997837b8bb7d8b3a1
kk_Ataka <kk_ataka@ring.skr.jp>
2013-07-18 07:59:01 +0900

    ひと通り動くようになった

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-redmine_auto_ticket_add
-=======================
+# auto_ticket_add
 
-Redmineプラグイン作成の練習
+## 役割
+
+最新コミットログにチケットナンバー( #1 とか)が入っている場合、該当するチケットの注記にコミットログを差し込んで更新するプラグイン。
+
+※ 履歴にコミットログを出し `Export csv with journals` と組み合わせて使いたい(「関係しているリビジョン」でも見れるけど、csvには出してくれないので)
+
+## 対応
+
+- Subversion
+
+## 必須
+
+- cURL
+
+## 動作環境
+
+```
+Environment:
+  Redmine version                          2.3.1.stable
+  Ruby version                             1.9.3 (i386-mingw32)
+  Rails version                            3.2.13
+  Environment                              development
+  Database adapter                         Mysql2
+```
+
+## 手順
+
+### プラグインインストール
+
+$ cd REDMINE_HOME/plugin                                    # Redmineのバージョンによって場所違う
+$ git clone https://github.com/gosyujin/auto_ticket_add.git
+
+### Redmine設定
+
+//  - `管理 => ロールと権限` から `Auto ticket add` にチェックを入れる
+//  - `プロジェクト => 設定 => モジュール` から `Auto ticket add` にチェックを入れる
+- `管理 => 設定 => 認証` から `RESTによるWebサービスを有効にする` にチェックを入れる
+- `個人設定` から `APIアクセスキー` を表示し、メモる`
+
+### Subversion設定
+
+- プロジェクトに設定しているリポジトリのフックスクリプトを編集
+
+$ cd REPOSITORY_HOME/hooks
+$ cp post-commit.tmpl post-commit.bat # Windowsの場合
+
+set http_proxy=
+curl -O -s -X GET -H "X-Redmine-API-Key: =82d33ec92eb0bf72390998a875d614a37fce29ec" http://localhost:3000/projects/testproject/auto_ticket/add?revision=%2
--- app/controllers/auto_ticket_controller.rb
+++ app/controllers/auto_ticket_controller.rb
@@ -1,4 +1,6 @@
+# -*- encoding: utf-8 -*-
 require 'nokogiri'
+require 'pp'
 
 class AutoTicketController < ApplicationController
   unloadable
@@ -9,64 +11,87 @@ class AutoTicketController < ApplicationController
   helper :issues
   include IssuesHelper
 
+  Git_Splitter = "|||"
+
   def index
     puts "call index"
-
-    log_xml = ""
-    repositories = @project.repositories
-    if repositories.empty? then
-      @nothing = "repository is nothing..."
-      return
-    end
-
-    repositories.each do |repository|
-       # only default scm
-       next unless repository["is_default"]
-
-       case repository["type"]
-       when Repository::Subversion.to_s
-         log = `svn log --xml #{repository.url}`
-         @log = parse(log)
-         @log.each do |revision, log|
-           tickets_no = log.scan(/#[1-9]*/)
-           next if tickets_no.empty?
-           tickets_no.each do |ticket_no|
-             puts "kono ticket no [#{ticket_no}] ni revision no log [r#{revision}: #{log}]"
-           end
-         end
-         return
-       when Repository::Git.to_s
-         splitter = "||"
-         log = {}
-         pre = `git --git-dir=#{repository.url} log --pretty=format:"%h#{splitter}%s"`
-         pre.split("\n").each do |l|
-           k,v = l.split(splitter)
-           log[k] = v
-         end
-         @log = log
-         return
-       else
-         @nothing = "It have support only Subversion"
-         return
-       end
-    end
-    @log = parse(log_xml)
+    commit_log
   end
 
   def add
     puts "call add"
+    commit_log
+    @log.each do |revision, log|
+      # scan commit log: if include issue no => "#n"
+      issues_id = log.scan(/#[1-9]*/)
+      next if issues_id.empty?
+
+      issues_id.each do |issue_id|
+        issue_id_only_num = issue_id.delete("#")
+
+        project_id = @project.id
+        issue = Issue.find(:first, :conditions => ["id = ?", issue_id_only_num])
+        issue.init_journal(User.current, "r#{revision}: #{log}")
+        success = issue.save
+        unless success then
+          @error_message
+        end
+      end
+    end
   end
 
 private
-  def parse(xml)
-    log = {}
-    doc = Nokogiri::XML(xml)
-    doc.xpath('/log/logentry').each do |item|
-      item.xpath('./msg').each do |msg|
-        log[item["revision"]] = msg.text
+  def commit_log
+    repositories = @project.repositories
+    if repositories.empty? then
+      @error_message = "repository is nothing..."
+      return
+    end
+
+    repositories.each do |repository|
+       # ONLY default scm exec
+       next unless repository["is_default"]
+
+       case repository["type"]
+       when Repository::Subversion.to_s
+         if params["revision"].nil? or params["revision"] == "" then
+           revision = "--limit 1"
+         else
+           revision = "-r #{params["revision"]}"
+         end
+
+         log = `svn log --xml #{repository.url} #{revision}`
+         @log = parse(:svn, log)
+         return
+       when Repository::Git.to_s
+
+         log = `git --git-dir=#{repository.url} log --pretty=format:"%h#{Git_Splitter}%s"`
+         @log = parse(:git, log)
+         return
+       else
+         @error_message = "It have support only Subversion"
+         return
+       end
+    end
+  end
+
+  def parse(scm, log)
+    hash = {}
+    case scm
+    when :svn
+      doc = Nokogiri::XML(log)
+      doc.xpath('/log/logentry').each do |item|
+        item.xpath('./msg').each do |msg|
+          hash[item["revision"]] = msg.text
+        end
+      end
+    when :git
+      log.split("\n").each do |lo|
+        k,v = lo.split(Git_Splitter)
+        hash[k] = v
       end
     end
-    return log
+    return hash
   end
 
   def find_project
