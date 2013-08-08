35935dd053fd7130ed5cd2291a00b222a70ffc5d
kk_Ataka <kk_ataka@ring.skr.jp>
2013-08-09 06:56:27 +0900

    テストスクリプト追加

---
+++ test/functional/auto_ticket_controller_test.rb
@@ -0,0 +1,11 @@
+require './' + File.dirname(__FILE__) + '/../test_helper'
+
+class AutoTicketControllerTest < ActionController::TestCase
+  test "the truth" do
+    assert true
+  end
+
+  test "access" do
+    get
+  end
+end
